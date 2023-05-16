1b5a130b22e241137128262424efe39b2fd4115d
Nick Hildebrant <nickhildebrant09@gmail.com>
2023-05-16 04:45:36 -0700

    Got the stopping working

--- Assets/Scripts/Train.cs
+++ Assets/Scripts/Train.cs
@@ -6,13 +6,15 @@ using UnityEngine;
 public class Train : MonoBehaviour
 {
     public float speed = 10f;
-    public float acceleration = 0.0f;
-    public bool isStop = false;
-    public Material redMaterial;
+    public bool isStopped = false;
+    public Material redMaterial, greenMaterial;
 
     public GameObject SendToObject;
 
     public Transform targetA, targetB;
+
+    private float acceleration = 0.0f;
+    public float timer = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,19 +25,41 @@ public class Train : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (isStop) return;
+        if(isStopped && timer >= 1.0f)
+        {
+            acceleration = 0.1f;
+            timer = 0.0f;
+            isStopped = false;
+        }
+
+        if (isStopped)
+        {
+            timer += Time.deltaTime;
+            return;
+        }
 
         speed += acceleration;
 
-        Vector3 directionVector = Vector3.Normalize(targetA.position - targetB.position);
+        Vector3 directionVector = Vector3.Normalize(targetB.position - targetA.position);
         transform.rotation = Quaternion.LookRotation(directionVector);
 
         transform.position += speed * directionVector * Time.deltaTime;
+
+        if (speed <= 0)
+        {
+            isStopped = true;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        acceleration = -0.0005f;
+        acceleration = -0.1f;
         SendToObject.GetComponent<Renderer>().material = redMaterial;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        SendToObject.GetComponent<Renderer>().material = greenMaterial;
+        acceleration = 0;
     }
 }
