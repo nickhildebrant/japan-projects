670821338dd768a73bcf13d0a6dc27624203a7dc
Nick Hildebrant <nickhildebrant09@gmail.com>
2023-05-25 10:20:48 -0700

    Reorganized

--- Assets/Scripts/Camera/CameraMove.cs
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
     public float movementSpeed = 5.0f;
+    public float rotateSpeed = 1.0f;
 
     // Update is called once per frame
     void Update()
@@ -15,11 +16,17 @@ public class CameraMove : MonoBehaviour
         Vector3 newPosition = transform.position;
         
         // Basic camera key controls
-        if(Input.GetKey(KeyCode.W)) { newPosition.z -= movementSpeed * Time.deltaTime; }
-        if(Input.GetKey(KeyCode.A)) { newPosition.x += movementSpeed * Time.deltaTime; }
-        if(Input.GetKey(KeyCode.S)) { newPosition.z += movementSpeed * Time.deltaTime; }
-        if(Input.GetKey(KeyCode.D)) { newPosition.x -= movementSpeed * Time.deltaTime; }
+        if(Input.GetKey(KeyCode.W)) { newPosition.z += movementSpeed * Time.deltaTime; }
+        if(Input.GetKey(KeyCode.A)) { newPosition.x -= movementSpeed * Time.deltaTime; }
+        if(Input.GetKey(KeyCode.S)) { newPosition.z -= movementSpeed * Time.deltaTime; }
+        if(Input.GetKey(KeyCode.D)) { newPosition.x += movementSpeed * Time.deltaTime; }
 
-        transform.position = newPosition;  // Updates the camera position
+        // Basic camera key controls
+        if (Input.GetKey(KeyCode.UpArrow)) { transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.Self); }
+        if (Input.GetKey(KeyCode.LeftArrow)) { transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.Self); }
+        if (Input.GetKey(KeyCode.DownArrow)) { transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.Self); }
+        if (Input.GetKey(KeyCode.RightArrow)) { transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.Self); }
+
+        transform.position = newPosition;
     }
 }
