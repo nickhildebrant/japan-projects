c19d5099fc1bc3920dd631836b489c0cc054b8cd
Nick Hildebrant <nickhildebrant09@gmail.com>
2023-05-25 12:00:32 -0700

    Building the Roads

--- Assets/Scripts/Camera/CameraMove.cs
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
     public float movementSpeed = 5.0f;
-    public float rotateSpeed = 1.0f;
+    public float rotateSpeed = 30.0f;
 
     // Update is called once per frame
     void Update()
@@ -16,16 +16,18 @@ public class CameraMove : MonoBehaviour
         Vector3 newPosition = transform.position;
         
         // Basic camera key controls
-        if(Input.GetKey(KeyCode.W)) { newPosition.z += movementSpeed * Time.deltaTime; }
-        if(Input.GetKey(KeyCode.A)) { newPosition.x -= movementSpeed * Time.deltaTime; }
-        if(Input.GetKey(KeyCode.S)) { newPosition.z -= movementSpeed * Time.deltaTime; }
-        if(Input.GetKey(KeyCode.D)) { newPosition.x += movementSpeed * Time.deltaTime; }
+        if(Input.GetKey(KeyCode.W)) { newPosition += transform.forward * movementSpeed * Time.deltaTime; }
+        if(Input.GetKey(KeyCode.A)) { newPosition -= transform.right * movementSpeed * Time.deltaTime; }
+        if(Input.GetKey(KeyCode.S)) { newPosition -= transform.forward * movementSpeed * Time.deltaTime; }
+        if(Input.GetKey(KeyCode.D)) { newPosition += transform.right * movementSpeed * Time.deltaTime; }
+        if(Input.GetKey(KeyCode.E)) { newPosition.y += movementSpeed * Time.deltaTime; }
+        if(Input.GetKey(KeyCode.Q)) { newPosition.y -= movementSpeed * Time.deltaTime; }
 
         // Basic camera key controls
-        if (Input.GetKey(KeyCode.UpArrow)) { transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.Self); }
-        if (Input.GetKey(KeyCode.LeftArrow)) { transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.Self); }
-        if (Input.GetKey(KeyCode.DownArrow)) { transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.Self); }
-        if (Input.GetKey(KeyCode.RightArrow)) { transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.Self); }
+        if (Input.GetKey(KeyCode.UpArrow)) { transform.Rotate(-transform.right * rotateSpeed * Time.deltaTime, Space.World); }
+        if (Input.GetKey(KeyCode.LeftArrow)) { transform.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime, Space.World); }
+        if (Input.GetKey(KeyCode.DownArrow)) { transform.Rotate(transform.right * rotateSpeed * Time.deltaTime, Space.World); }
+        if (Input.GetKey(KeyCode.RightArrow)) { transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.World); }
 
         transform.position = newPosition;
     }
